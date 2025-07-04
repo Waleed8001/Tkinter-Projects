c4d8ce399b1ef4409283911cb0f9791a1662d856
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2025-07-04 19:06:14 +0500

    Delete Program-3 directory

--- Program-3/3_quiz_lecture_6.py
+++
@@ -1,9 +0,0 @@
-from tkinter import*
-
-root = Tk()
-root.geometry("500x400")
-
-strip_label = Label(root, text="ready", font=("Times New Roman", 10), relief=SUNKEN, bg="red", fg="white")
-strip_label.pack(side=BOTTOM, anchor=CENTER, fill=X)
-
-root.mainloop()
